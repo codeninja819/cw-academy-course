f93f1e7906472e72582b91695bcfbaaea099607f
codeninja819 <jiromatsumoto98@gmail.com>
2023-11-16 22:20:26 -0500

    lesson 3

--- counting_contract/src/lib.rs
+++ counting_contract/src/lib.rs
@@ -1,15 +1,27 @@
 use cosmwasm_std::{
-    DepsMut, Env, MessageInfo, Empty, StdResult, Response, entry_point
+    Deps, DepsMut, Env, MessageInfo, Empty, StdResult, Response, entry_point, to_json_binary
 };
-
+use cosmwasm_std::Binary;
+ 
+mod contract;
 pub mod msg;
-
+ 
 #[entry_point]
 pub fn instantiate(
-    _deps: DepsMut,
-    _env: Env,
-    _info: MessageInfo,
-    _msg: Empty,
+	_deps: DepsMut,
+	_env: Env,
+	_info: MessageInfo,
+	_msg: Empty,
 ) -> StdResult<Response> {
-    Ok(Response::new())
+	Ok(Response::new())
+}
+ 
+#[entry_point]
+pub fn query(_deps: Deps, _env: Env, msg: msg::QueryMsg) -> StdResult<Binary> {
+    use msg::QueryMsg::*;
+    use contract::query;
+ 
+    match msg {
+        Value {} => to_json_binary(&query::value()),
+    }
 }
